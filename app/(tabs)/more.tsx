5bcbc3abf5752c46695ce4a79f7e2270dd2eac95
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2025-10-31 16:29:08 +0530

    iconification

--- app/(tabs)/more.tsx
+++ app/(tabs)/more.tsx
@@ -1,4 +1,6 @@
 // app/(tabs)/more.tsx
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { useRouter } from 'expo-router';
 import { Text, TouchableOpacity, View } from 'react-native';
 
@@ -8,16 +10,18 @@ export default function MoreScreen() {
     return (
         <View className="flex-1 bg-gray-50 p-4 gap-4">
             <TouchableOpacity
-                className="bg-white p-6 rounded-xl"
-                onPress={() => router.push('/guess')} // or keep in tabs
+                className="bg-white p-6 rounded-xl flex flex-row justify-between "
+                onPress={() => router.push('/guess')}
             >
                 <Text className="text-xl font-semibold">Guess Pokémon</Text>
+                <FontAwesome5 name="question" size={24} color="black" />
             </TouchableOpacity>
             <TouchableOpacity
-                className="bg-white p-6 rounded-xl"
+                className="bg-white p-6 rounded-xl flex flex-row justify-between "
                 onPress={() => router.push('/quiz')}
             >
                 <Text className="text-xl font-semibold">Pokémon Quiz</Text>
+                <MaterialIcons name="quiz" size={24} color="black" />
             </TouchableOpacity>
         </View>
     );
